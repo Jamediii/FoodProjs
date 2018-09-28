c18df874a733a36e6c5d738ecb7c47f36e92f1bf
sjm <1442808944@qq.com>
2018-09-28 21:35:38 +0800

    文章板块：     文章：           删：根据id删除文章           查：显示所有文章的全部信息/根据id获取文章全部信息、简介信息；模糊查询（根据name）               根据分类classifyid获取该分类下的文章id           排序：根据文章点赞量排序     作者：           删：根据id删除作者           查：显示所有作者的全部信息、根据id获取作者全部信息；模糊查询（根据name） 菜谱板块：           删：根据id删除菜谱           查：显示所有菜谱的简介信息、根据id获取菜谱全部信息、简介信息；模糊查询（根据name）               根据分类recipesclassifyid获取该分类下的菜谱id               用户菜谱：获取用户上传且通过审核的全部菜谱的简介信息、根据id获取用户上传的，通过审核的菜谱全部信息、简介信息           排序：根据菜谱点赞量排序

---
+++ controllers/communityControllers.js
@@ -0,0 +1,139 @@
+const communityDAO = require('../model/communityDAO');
+
+module.exports = {
+    //文章
+    // addArticle:async (ctx,next)=>{
+    //     let article = {
+    //         "articleName": ctx.request.body.articleName,
+    //         "articleContent": ctx.request.body.articleContent,
+    //         "articlePicBig": ctx.request.body.articlePicBig,
+    //         "articlePicSmall": ctx.request.body.articlePicSmall,
+    //         "articleBrief":ctx.request.body.articleBrief
+    //     };
+    //     try {
+    //         await communityDAO.addArticle(article);
+    //         ctx.body = {"code":200,"message":"ok",data:[]}
+    //     }catch (err) {
+    //         ctx.body = {"code":500,"message":err.toString(),data:[]}
+    //     }
+    // },
+    getAllArticle:async (ctx,next)=>{
+        try {
+            let jsondata = await communityDAO.getAllArticle();
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+
+    getOneArticle:async (ctx,next)=>{
+        try {
+            let jsondata = await communityDAO.getOneArticle(ctx.params.articleId);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    getOneArticleBrief:async (ctx,next)=>{
+        try {
+            let jsondata = await communityDAO.getOneArticleBrief(ctx.params.articleId);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    delOneArticle:async (ctx,next)=>{
+        try {
+            await communityDAO.delOneArticle(ctx.params.articleId);
+            ctx.body = {"code":200,"message":"删除成功",data:[]}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    findArticle:async (ctx,next)=>{
+        try {
+            let p_articleName = ctx.request.body.p_articleName;
+            let jsondata = await communityDAO.findArticle(p_articleName);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    classifyArticle:async (ctx,next)=>{
+        try{
+            let classifyId = ctx.params.classifyId;
+            let jsondata = await communityDAO.classifyArticle(classifyId);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    orderArticle:async (ctx,next)=>{
+        try{
+            let jsondata = await communityDAO.orderArticle();
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+
+    //作者
+    getAllAuthor:async (ctx,next)=>{
+        try{
+            let jsondata = await communityDAO.getAllAuthor();
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    addAuthor:async (ctx,next)=>{
+        try {
+            let author = {
+                "authorName": ctx.request.body.authorName,
+                "authorBrief": ctx.request.body.authorBrief,
+                "authorImg":ctx.request.body.authorImg
+            };
+            await communityDAO.addAuthor(author);
+            ctx.body = {"code":200,"message":"ok",data:[]}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    getOneAuthor:async (ctx,next)=>{
+        try {
+            let jsondata = await communityDAO.getOneAuthor(ctx.params.authorId);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    delOneAuthor:async (ctx,next)=>{
+        try {
+            await communityDAO.delOneAuthor(ctx.params.authorId);
+            ctx.body = {"code":200,"message":"删除成功",data:[]}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    findAuthor:async (ctx,next)=>{
+        try {
+            let p_authorName = ctx.request.body.p_authorName;
+            let jsondata = await communityDAO.findAuthor(p_authorName);
+            ctx.body = {"code":200,"message":"ok",data:jsondata}
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    }
+    // updateOneAuthor:async (ctx,next)=>{
+    //     try{
+    //         let author = {
+    //             "authorName":ctx.params.authorName,
+    //             "authorId":ctx.params.authorId
+    //         };
+    //         await communityDAO.updateOneAuthor(author);
+    //         ctx.body = {"code":200,"message":"ok",data:[]}
+    //     }catch (err) {
+    //         ctx.body = {"code":500,"message":err.toString(),data:[]}
+    //     }
+    // }
+}
